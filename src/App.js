d1d997cd9d1a6127396e591e1c7b7b631863ad9c
Anjani <akanjaniuuu@gmail.com>
2020-11-29 21:57:59 +0530

    latest commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
-
+import React, { useState } from 'react';
 function App() {
+
+  const [Hori, setHori] = useState(10)
+  const [Veri, setVeri] = useState(10)
+  const [Blur, setBlur] = useState(10)
+  const[clr, setclr] = useState("black")
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="controls">
+          <label>Horizontal Length</label>  
+          <input type="range" min="-100" max="100" value={Hori} onChange ={(e) => setHori(e.target.value)}/> <br /> <br />
+          <label>Vertical Length</label>  
+          <input type="range" min="-100" max="100" value={Veri} onChange ={(e) => setVeri(e.target.value)}/> <br/> <br />
+          <label>Blur Range</label>  
+          <input type="range" min="0" max="100" value={Blur} onChange ={(e) => setBlur(e.target.value)}/> <br/> <br />
+          <label>set background Colour</label>  
+          <input type= "color" value={clr} value={clr} onChange ={(e) => setclr(e.target.value)}/>
+      </div>
+    
+    
+    <div className="output">
+      <div className="box" style={{boxShadow:`${Hori}px ${Veri}px ${Blur}px ${clr}`}}>
+        
+      </div>
     </div>
+  </div>
   );
 }
 
